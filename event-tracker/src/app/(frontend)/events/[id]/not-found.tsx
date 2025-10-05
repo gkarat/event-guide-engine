3e1a82a5d4ea0fd36478b76651eeda7c3da616d7
Georgii Karataev <georgkarat@gmail.com>
2025-10-05 15:28:49 +0200

    fix: Fix wrong EventNotFound directory location

--- event-tracker/src/app/(frontend)/events/[id]/not-found.tsx
+++ event-tracker/src/app/(frontend)/events/[id]/not-found.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import EventNotFound from '@/collections/components/EventNotFound/EventNotFound'
+import EventNotFound from '@/components/EventNotFound/EventNotFound'
 
 const NotFound: React.FC = () => {
   return <EventNotFound />
